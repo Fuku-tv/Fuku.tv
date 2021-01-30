074e131db7c32628b2e7db1033ae83795c3fe6e0
olivajames110 <37842613+olivajames110@users.noreply.github.com>
2021-01-29 21:54:11 -0500

    Button folder fix

--- workspaces/client/src/components/Main/ContentSection/Tabs/PlayGame/ControlsSection/DropClawButton/DropClawButton.tsx
+++ workspaces/client/src/components/Main/ContentSection/Tabs/PlayGame/ControlsSection/DropClawButton/DropClawButton.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useGameState } from 'src/state/hooks';
-import DepthButton from 'src/shared/components/UIElements/DepthButton_OLD/DepthButton';
+import DepthButton from 'src/shared/components/UIElements/DepthButton/DepthButton';
 
 import './DropClawButton.scss';
 
