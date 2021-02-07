753723c3730b546a0b4b8220cb0bedef1127c027
blackfenix2 <devin_98@msn.com>
2021-02-07 16:09:37 -0500

    testing out self signed certificates.

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -1,10 +1,18 @@
-import http from 'http';
-
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 import { ControllerServer } from './viewerControllerServer';
 
 import { VideoServer } from './viewerVideoServer';
 
-const controllerHttpsServer = http.createServer((req, res) => {
+var privateKey = fs.readFileSync(path.resolve(__dirname, '../certs/key.pem'), 'utf8');
+var certificate = fs.readFileSync(path.resolve(__dirname, '../certs/cert.pem'), 'utf8');
+var credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
+const controllerHttpsServer = https.createServer(credentials, (req, res) => {
   res.writeHead(200);
   res.write('ok, good here!');
   res.end();
