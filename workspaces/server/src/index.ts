803e0e1bb5d87a42e87cdbe9d56dddf897c1cc03
blackfenix2 <devin_98@msn.com>
2021-02-07 04:49:39 -0500

    trying port 8080 for websocket.

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -5,10 +5,10 @@ import { ControllerServer } from './viewerControllerServer';
 import { VideoServer } from './viewerVideoServer';
 
 const controllerHttpsServer = http.createServer();
-controllerHttpsServer.listen(10888);
+controllerHttpsServer.listen(8080);
 
-const videoHttpsServer = http.createServer();
-videoHttpsServer.listen(10889);
+// const videoHttpsServer = http.createServer();
+// videoHttpsServer.listen(10889);
 
 new ControllerServer(controllerHttpsServer);
-new VideoServer(videoHttpsServer);
+// new VideoServer(videoHttpsServer);
