e3a629f1f324646622a5614bcca2788695f05b09
blackfenix2 <devin_98@msn.com>
2021-02-11 13:55:30 -0500

    another commit to fix AWS build

--- workspaces/client/src/index.ts
+++ workspaces/client/src/index.ts
@@ -20,14 +20,6 @@ window.addEventListener(`load`, () => {
   });
 });
 
-// unregister all service workers
-// navigator.serviceWorker.getRegistrations().then((registrations) => {
-//   for (const registration of registrations) {
-//     registration.unregister();
-//     console.log('unregistered serviceworker: ', registration.active);
-//   }
-// });
-
 if ((module as HotModule).hot) {
   (module as HotModule).hot.accept();
 }
