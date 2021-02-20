fe7670b164f7a44c646d095e726f1ffc05b3b84a
blackfenix2 <devin_98@msn.com>
2021-02-20 12:26:48 -0500

    added typings to webpack.config.js

--- workspaces/server/webpack.config.js
+++ workspaces/server/webpack.config.js
@@ -1,5 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+
 const { NODE_ENV = 'production' } = process.env;
+
+/** @type {import("webpack").Configuration } */
 module.exports = {
   entry: {
     bundle: './src/index.ts',
