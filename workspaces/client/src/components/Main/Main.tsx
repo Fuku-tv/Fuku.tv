5da820b3adebe0fa60bac9609a14afee211265b0
olivajames110 <olivajames110@gmail.com>
2021-02-25 09:42:42 -0500

    Prize items

--- workspaces/client/src/components/Main/Main.tsx
+++ workspaces/client/src/components/Main/Main.tsx
@@ -3,6 +3,8 @@ import { isMobile } from 'react-device-detect';
 
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import useAuthState from 'src/state/hooks/useAuthState';
+import { useGameState } from 'src/state/hooks';
+
 import Header from '../Header/Header';
 import AboutScreen from '../Screens/AboutScreen/AboutScreen';
 import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
@@ -43,34 +45,35 @@ const Routes = () => (
   </Switch>
 );
 
-const playerStats = (
-  <div className="player-stats-container">
-    <ContentContainer>
-      <div className="inner-wrapper">
-        <div id="points-and-credits">
-          <div className="stat-item-wrapper">
-            <span className="player-stats__item">Credits:</span>
-            <span className="player-stats__value">15</span>
-          </div>
-          <div className="stat-item-wrapper">
-            <span className="player-stats__item">Points:</span>
-            <span className="player-stats__value">1540</span>
-          </div>
-        </div>
-        <div id="level">
-          <div className="stat-item-wrapper">
-            <span className="player-stats__item">Level:</span>
-            <span className="player-stats__value">6</span>
-          </div>
-          <progress min="0" max="100" value="63" />
-        </div>
-      </div>
-    </ContentContainer>
-  </div>
-);
-
 const Main: React.FC = () => {
   const { state, actions } = useAuthState();
+  const gameState = useGameState();
+  const playerStats = (
+    <div className="player-stats-container">
+      <ContentContainer>
+        <div className="inner-wrapper">
+          <div id="points-and-credits">
+            <div className="stat-item-wrapper">
+              <span className="player-stats__item">Credits:</span>
+              <span className="player-stats__value">{gameState.state.credits}</span>
+            </div>
+            <div className="stat-item-wrapper">
+              <span className="player-stats__item">Points:</span>
+              <span className="player-stats__value">1540</span>
+            </div>
+          </div>
+          <div id="level">
+            <div className="stat-item-wrapper">
+              <span className="player-stats__item">Level:</span>
+              <span className="player-stats__value">6</span>
+            </div>
+            <progress min="0" max="100" value="63" />
+          </div>
+        </div>
+      </ContentContainer>
+    </div>
+  );
+
   return (
     <main>
       {state.isAuthenticated && playerStats}
