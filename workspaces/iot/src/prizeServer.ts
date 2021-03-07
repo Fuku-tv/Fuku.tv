825fcc337aef2cd1e3f9071e558f6ba7b305e2cf
Jason Pullara <lordkat@lordkat.com>
2021-03-06 19:16:21 -0500

    Update prizeServer.ts

--- workspaces/iot/src/prizeServer.ts
+++ workspaces/iot/src/prizeServer.ts
@@ -11,15 +11,17 @@ const ioPin = new gpio(19, 'in', 'both');
 
 let lastVal = 0;
 
-ioPin.watch((err: any, val: any) => {
-  if (err) {
-    logger.log(LogLevel.error, 'ioPin error: ' + err);
-    return;
-  }
-  if (val === 1 && lastVal === 0) {
-    logger.log(LogLevel.info, 'prize get');
-    serial.write('1');
-    lastVal = val;
-  }
-  else lastVal = 0;
+serial.open(() => {
+  ioPin.watch((err: any, val: any) => {
+    if (err) {
+      logger.log(LogLevel.error, 'ioPin error: ' + err);
+      return;
+    }
+    if (val === 1 && lastVal === 0) {
+      logger.log(LogLevel.info, 'prize get');
+      serial.write('1');
+      lastVal = val;
+    }
+    else lastVal = 0;
+  });
 });
