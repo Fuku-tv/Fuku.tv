e375c87de3d16e4214f9fbd03df83b0c4dfce8b4
blackfenix2 <devin_98@msn.com>
2021-03-25 23:14:53 -0400

    added agcod package

--- workspaces/client/src/services/giftCardService.ts
+++ workspaces/client/src/services/giftCardService.ts
@@ -1,3 +1,32 @@
-const ACCESS_KEY = 'AKIAWWFS6UKLOKZ7UTLW';
+import axios from 'axios';
 
-const SECRET_KEY = '2XeDFaF9dfQjlmRIb+DzvpRBGAupeljzV2cYe/8C';
+import Client from 'agcod';
+
+const REGION = 'NA';
+
+const CURRENCY = 'USD';
+
+const config = {
+  endpoint: {
+    NA: {
+      host: 'agcod-v2-gamma.amazon.com',
+      region: 'us-east-1',
+      countries: ['US', 'CA'],
+    },
+  },
+  partnerId: 'Fu863',
+  credentials: {
+    accessKeyId: 'AKIAWWFS6UKLOKZ7UTLW',
+    secretAccessKey: '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',
+  },
+};
+
+const client = new Client(config);
+
+export const createGiftCard = () => {
+  client.createGiftCard('NA', 123, 'USD', (error, result) => {
+    console.log('client.createGiftCard response', error, result);
+  });
+};
+
+export const validateGiftCard = () => {};
