4bebf69f82bff7aac144f9a3b22eb9a3e3cedbfc
blackfenix2 <devin_98@msn.com>
2021-04-05 18:52:49 -0400

    added pointsToRemove method added logic to prevent gift card generation on insufficent points.

--- workspaces/lambda/src/http/giftCard.ts
+++ workspaces/lambda/src/http/giftCard.ts
@@ -2,6 +2,7 @@ import type { APIGatewayProxyHandler } from 'aws-lambda';
 import Client from 'agcod';
 import { env } from 'fuku.tv-shared';
 import validateUser from 'src/common/authorizer';
+import { playersTableModel } from 'fuku.tv-shared/dynamodb/table';
 import { sendEmail } from '../common/emailService';
 import * as Responses from '../common/ApiResponses';
 
@@ -36,6 +37,7 @@ export const index: APIGatewayProxyHandler = async (event, context, callback) =>
   const { domainName, stage, identity } = event.requestContext;
   const res = JSON.parse(event.body);
 
+  // validate user
   let email: string;
   try {
     const user = await validateUser(event.headers.Authorization);
@@ -44,7 +46,9 @@ export const index: APIGatewayProxyHandler = async (event, context, callback) =>
     return Responses.badRequest({ message: 'User could not be validated' });
   }
 
+  // deduct points and send customer redemption code
   try {
+    playersTableModel.removePoints(email, 2000);
     // TODO get gift card amount from request body
     const claimCode = await createGiftCard(res.amount);
     await sendEmail('support@fuku.tv', email, 'Fuku Redemption', claimCode);
