aa08f3ddca5230c89d89209167b0fc7197a46ec3
blackfenix2 <devin_98@msn.com>
2021-04-17 02:24:42 -0400

    hotfix

--- workspaces/shared/env.ts
+++ workspaces/shared/env.ts
@@ -83,10 +83,11 @@ export const stripeApiKey = (): string => {
   let value: string;
   // check for live key in ENV
   if (process.env.EB_ENVIRONMENT === 'production' || process.env.LAMBDA_ENV === 'prod') {
-    getSecrets().then((data) => {
-      value = data.STRIPE_API_KEY;
-    });
-    return value;
+    return process.env.STRIPE_API_KEY;
+    // getSecrets().then((data) => {
+    //   value = data.STRIPE_API_KEY;
+    // });
+    // return value;
   }
   // return default test key
   return 'pk_test_51HxGG6Gx8BmO5evBcDbYjClczRZa0rC96ZiA3ZFyn5ErewXeH2TgAs9cseKW6mT1mMpfRepbtbXEgrPEWovaHbn100wlrLXvff';
@@ -113,10 +114,12 @@ export const stripeWebhookSecret = (): string => {
 
   // check for live key in ENV
   if (process.env.EB_ENVIRONMENT === 'production' || process.env.LAMBDA_ENV === 'prod') {
-    getSecrets().then((data) => {
-      value = data.STRIPE_WEBHOOK_SECRET;
-    });
-    return value;
+    // getSecrets().then((data) => {
+    //   value = data.STRIPE_WEBHOOK_SECRET;
+    // });
+    // return value;
+
+    return 'whsec_wEti55fBXOdPv34e9RHdzEcLDuufuBJv';
   }
   // return test secret
   return 'whsec_HBf2DDCg0jGYhdrJ4smIGkDUuFAZ8Wd8';
@@ -127,10 +130,11 @@ export const amazonGiftCardKey = (): string => {
 
   // check for live key in ENV
   if (process.env.EB_ENVIRONMENT === 'production' || process.env.LAMBDA_ENV === 'prod') {
-    getSecrets().then((data) => {
-      value = data.AMAZON_GIFTCARD_KEY;
-    });
-    return value;
+    // getSecrets().then((data) => {
+    //   value = data.AMAZON_GIFTCARD_KEY;
+    // });
+    // return value;
+    return 'AKIAZUTR4R2X6527A5VZ';
   }
   // return test secret
   return 'AKIAWWFS6UKLIFJK3XO4';
@@ -141,10 +145,11 @@ export const amazonGiftCardSecret = (): string => {
 
   // check for live key in ENV
   if (process.env.EB_ENVIRONMENT === 'production' || process.env.LAMBDA_ENV === 'prod') {
-    getSecrets().then((data) => {
-      value = data.AMAZON_GIFTCARD_SECRET;
-    });
-    return value;
+    // getSecrets().then((data) => {
+    //   value = data.AMAZON_GIFTCARD_SECRET;
+    // });
+    // return value;
+    return 'UeBUTm+WhYBubN8aweZwm+PTLR4SZhCxUjWRjZYu';
   }
   // return test secret
   return '/F4WVa8cJAmEyGedETdQiDYCclV/yULb5tlKjgmw';
