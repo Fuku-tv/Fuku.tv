6a88d8efcc6d64ec9d217f0d814eb07b33ca25c2
blackfenix2 <devin_98@msn.com>
2021-04-20 17:59:40 -0400

    refactored dynamodb models adds default values for missing properties.

--- workspaces/shared/dynamodb/table.ts
+++ workspaces/shared/dynamodb/table.ts
@@ -1,20 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
 import dynamo from './dynamo';
-import { Player, PointsLedger, Replay } from './models';
+import { Player, PointsLedger, Replay, defaults, BaseModel } from './models';
 import { tableList } from './index';
 
+export const genericTableMethods = <T extends BaseModel>(tableName: string) => ({
+  async write(data: T): Promise<void> {
+    return dynamo.write<T>(data, tableName);
+  },
+  async get(id: string): Promise<T> {
+    return dynamo.get<T>(id, tableName);
+  },
+  async delete(id: string) {
+    return dynamo.delete(id, tableName);
+  },
+});
 // Create the DynamoDB service object
 
 export const playersTableModel = {
-  async write(data: Player): Promise<void> {
-    return dynamo.write<Player>(data, tableList.PLAYERS_TABLE);
-  },
-  async get(id: string): Promise<Player> {
-    return dynamo.get<Player>(id, tableList.PLAYERS_TABLE);
-  },
-  async delete(id: string) {
-    return dynamo.delete(id, tableList.PLAYERS_TABLE);
-  },
+  ...genericTableMethods<Player>(tableList.PLAYERS_TABLE),
 
   async removeCredits(id: string, creditsToRemove: number) {
     const player = await playersTableModel.get(id);
@@ -27,26 +30,21 @@ export const playersTableModel = {
 
   async addCredits(id: string, creditsToAdd: number) {
     const player = await playersTableModel.get(id);
-    if (player.credits === undefined)
-      player.credits = creditsToAdd;
-    else
-      player.credits += creditsToAdd;
+    if (player.credits === undefined) player.credits = creditsToAdd;
+    else player.credits += creditsToAdd;
     await playersTableModel.write(player);
   },
 
   async addPoints(id: string, pointsToAdd: number) {
     const player = await playersTableModel.get(id);
-    if (player.points === undefined)
-      player.points = pointsToAdd;
-    else
-      player.points += pointsToAdd;
+    if (player.points === undefined) player.points = pointsToAdd;
+    else player.points += pointsToAdd;
     await playersTableModel.write(player);
   },
 
   async removePoints(id: string, pointsToRemove: number) {
     const player = await playersTableModel.get(id);
-    if (player.points === undefined)
-      player.points = 0;
+    if (player.points === undefined) player.points = 0;
     if (player.points < pointsToRemove) {
       throw new Error('Insufficent points');
     }
@@ -67,8 +65,7 @@ export const playersTableModel = {
 
   async removeFreeplay(id: string, freeplayToRemove: number) {
     const player = await playersTableModel.get(id);
-    if (player.freeplay === undefined)
-      player.freeplay = 0;
+    if (player.freeplay === undefined) player.freeplay = 0;
     if (player.freeplay < freeplayToRemove) {
       throw new Error('Insufficient freeplay');
     }
@@ -78,10 +75,8 @@ export const playersTableModel = {
 
   async addFreeplay(id: string, freeplayToAdd: number) {
     const player = await playersTableModel.get(id);
-    if (player.freeplay === undefined)
-      player.freeplay = freeplayToAdd;
-    else
-      player.freeplay += freeplayToAdd;
+    if (player.freeplay === undefined) player.freeplay = freeplayToAdd;
+    else player.freeplay += freeplayToAdd;
     await playersTableModel.write(player);
   },
 
@@ -93,27 +88,9 @@ export const playersTableModel = {
 };
 
 export const replayTableModel = {
-  async write(data: Replay): Promise<void> {
-    return dynamo.write<Replay>(data, tableList.REPLAY_TABLE);
-  },
-  async get(id: string): Promise<Replay> {
-    return dynamo.get<Replay>(id, tableList.REPLAY_TABLE);
-  },
-  async delete(id: string) {
-    return dynamo.delete(id, tableList.REPLAY_TABLE);
-  },
+  ...genericTableMethods<Replay>(tableList.REPLAY_TABLE),
 };
 
 const pointsLedgerTableModel = {
-  async write(data: PointsLedger): Promise<void> {
-    return dynamo.write<PointsLedger>(data, tableList.POINTS_LEDGER_TABLE);
-  },
-  async get(id: string): Promise<PointsLedger> {
-    return dynamo.get<PointsLedger>(id, tableList.POINTS_LEDGER_TABLE);
-  },
-  async delete(id: string) {
-    return dynamo.delete(id, tableList.POINTS_LEDGER_TABLE);
-  },
+  ...genericTableMethods<PointsLedger>(tableList.POINTS_LEDGER_TABLE),
 };
-
-export const createTable = () => {};
