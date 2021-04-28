cff9015afb491cae5eb4a2841629be69deb9a8dc
blackfenix2 <devin_98@msn.com>
2021-04-28 13:33:38 -0400

    hotfix: dev stripe keys in production.

--- workspaces/shared/secrets/getSecret.ts
+++ workspaces/shared/secrets/getSecret.ts
@@ -4,8 +4,9 @@ export const stripeApiKey = async (): Promise<string> => {
   try {
     return (await getSecrets()).STRIPE_API_KEY;
   } catch (error) {
+    console.log(process.env.STRIPE_API_KEY);
     return (
-      process.env?.STRIPE_API_KEY || 'pk_test_51HxGG6Gx8BmO5evBcDbYjClczRZa0rC96ZiA3ZFyn5ErewXeH2TgAs9cseKW6mT1mMpfRepbtbXEgrPEWovaHbn100wlrLXvff'
+      process.env.STRIPE_API_KEY || 'pk_test_51HxGG6Gx8BmO5evBcDbYjClczRZa0rC96ZiA3ZFyn5ErewXeH2TgAs9cseKW6mT1mMpfRepbtbXEgrPEWovaHbn100wlrLXvff'
     );
   }
 };
@@ -16,7 +17,7 @@ export const stripeApiSecret = async (): Promise<string> => {
   } catch (error) {
     console.log(process.env.STRIPE_API_SECRET);
     return (
-      process.env?.STRIPE_API_SECRET || 'rk_test_51HxGG6Gx8BmO5evBLmxbuvgdsXyOf6BJLQKlzl5lEzFTBi1lUFixP09FJ6dPZUeWXzjn2cTF73zDVnTjGQEOqcH300qsohCbx9'
+      process.env.STRIPE_API_SECRET || 'rk_test_51HxGG6Gx8BmO5evBLmxbuvgdsXyOf6BJLQKlzl5lEzFTBi1lUFixP09FJ6dPZUeWXzjn2cTF73zDVnTjGQEOqcH300qsohCbx9'
     );
   }
 };
