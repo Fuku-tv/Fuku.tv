4801f06af70ebf6064f1ca81f05b8d8c4b4b1565
blackfenix2 <devin_98@msn.com>
2021-06-02 17:34:11 -0400

    added quick logging to scheduled freeplay lambda.

--- workspaces/lambda/src/trigger/scheduled/updateFreeplay.ts
+++ workspaces/lambda/src/trigger/scheduled/updateFreeplay.ts
@@ -5,7 +5,9 @@ const FREEPLAY_COUNT = 2;
 
 export const index: ScheduledHandler = async () => {
   const playerIdList = await playersTableModel.getList();
+  console.log('Found players to update: ', ...playerIdList);
   playerIdList.forEach(async (id) => {
+    console.log(`added ${FREEPLAY_COUNT} freeplay points to user ${id}`);
     await playersTableModel.addFreeplay(id, FREEPLAY_COUNT);
   });
 };
