6f03e38a5628a43c38e1d4cd91d1475b71a3e449
blackfenix2 <devin_98@msn.com>
2021-06-03 18:44:29 -0400

    added error check for updateFreeplay

--- workspaces/lambda/src/trigger/scheduled/updateFreeplay.ts
+++ workspaces/lambda/src/trigger/scheduled/updateFreeplay.ts
@@ -4,12 +4,16 @@ import { playersTableModel } from 'fuku.tv-shared/dynamodb/table';
 const FREEPLAY_COUNT = 2;
 
 export const index: ScheduledHandler = async () => {
-  const playerIdList = await playersTableModel.getList();
-  console.log('Found players to update: ', ...playerIdList);
-  playerIdList.forEach(async (id) => {
-    console.log(`added ${FREEPLAY_COUNT} freeplay points to user ${id}`);
-    await playersTableModel.addFreeplay(id, FREEPLAY_COUNT);
-  });
+  try {
+    const playerIdList = await playersTableModel.getList();
+    console.log('Found players to update: ', ...playerIdList);
+    playerIdList.forEach(async (id) => {
+      await playersTableModel.addFreeplay(id, FREEPLAY_COUNT);
+      console.log(`added ${FREEPLAY_COUNT} freeplay points to user ${id}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default index;
