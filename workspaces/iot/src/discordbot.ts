fc316ec9447005291f47c791f1fb907a3ba1bea6
Jason Pullara <lordkat@lordkat.com>
2021-06-08 13:42:47 -0400

    Update discordbot.ts

--- workspaces/iot/src/discordbot.ts
+++ workspaces/iot/src/discordbot.ts
@@ -1,12 +1,12 @@
-import {Client, Message} from 'discord';
+import * as Discord from 'discord';
 
-let discordClient = new Client();
+let discordClient = new Discord.Client();
 
 discordClient.login('ODQ5Njk4ODc2OTEwMjA2OTk3.YLe9vg.Yuwf32Ge2dFxw1ev92BZ6WygQqU');
 discordClient.on('ready', () => {
   console.log('Discord ready');
 });
-discordClient.on('message', (msg: Message) => {
+discordClient.on('message', (msg: Discord.Message) => {
   if (msg.content === 'ping') {
     msg.channel.send('pong');
     msg.channel.send('channel: ' + msg.channel);
