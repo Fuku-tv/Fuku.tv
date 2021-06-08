52ebb538a711364561b6dd252f19405a41257f8c
blackfenix2 <devin_98@msn.com>
2021-06-07 22:09:22 -0400

    hotfix, async/await within if statement doesnt work in lambda

--- workspaces/lambda/src/trigger/scheduled/updateFreeplay.ts
+++ workspaces/lambda/src/trigger/scheduled/updateFreeplay.ts
@@ -8,11 +8,11 @@ export const index: ScheduledHandler = async () => {
     const playerIdList = await playersTableModel.getList(['id', 'freeplay']);
     console.log(`Found ${playerIdList.length} players to update`);
     playerIdList.forEach(async (player) => {
-      if (player?.freeplay > 20) {
-        console.log(`Player ${player.id} already has ${player.freeplay} points in their account, skipping`);
-      } else {
-        await playersTableModel.addFreeplay(player.id, FREEPLAY_COUNT);
+      if (player?.freeplay < 20) {
         console.log(`added ${FREEPLAY_COUNT} freeplay points to user ${player.id}`);
+        playersTableModel.addFreeplay(player.id, FREEPLAY_COUNT);
+      } else {
+        console.log(`Player ${player.id} already has ${player.freeplay} points in their account, skipping`);
       }
     });
   } catch (error) {
