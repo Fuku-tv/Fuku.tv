e0bf0937d6bb3e2eb2041ce0a0f707306c9b2a8c
Jason Pullara <lordkat@lordkat.com>
2021-06-10 10:59:47 -0400

    discord bot
    discord bot

--- workspaces/server/src/discordBot.ts
+++ workspaces/server/src/discordBot.ts
@@ -1,7 +1,8 @@
 import { LogLevel, LoggerClass, env } from 'fuku.tv-shared';
 import * as redis from 'redis';
-
 import * as Discord from 'discord.js';
+import { playersTableModel } from '../shared/dynamodb/table';
+import { Player as PlayerModel } from '../shared/dynamodb/models';
 
 const DISCORD_TOKEN = 'ODQ5Njk4ODc2OTEwMjA2OTk3.YLe9vg.Yuwf32Ge2dFxw1ev92BZ6WygQqU';
 const DISCORD_CHANNEL_ID_DEBUG = '850164433111089152';
@@ -27,14 +28,25 @@ export class DiscordBot {
     });
 
     this.redisSubscriber.on('message', (channel: any, data: any) => {
+      if (this.isOnline === false) {
+        return;
+      }
+
       const { message } = JSON.parse(data);
       console.log(`discordbot got message: ${message}`);
-      console.log(`this.isonline: ${this.isOnline}`);
-      if (this.isOnline === true) {
+      if (channel === 'discordmessage') {
         (this.discordClient.channels.cache.get(DISCORD_CHANNEL_ID_DEBUG) as Discord.TextChannel).send(`${message.username}: ${message.chatmessage}`);
+      }
+      else if (channel === 'prizemessage') {
+        if (message.jackpot === false) {
+          (this.discordClient.channels.cache.get(DISCORD_CHANNEL_ID_DEBUG) as Discord.TextChannel).send(`${message.username} just scored ${message.points}!`);
+        } else {
+          (this.discordClient.channels.cache.get(DISCORD_CHANNEL_ID_DEBUG) as Discord.TextChannel).send(`${message.username} WON THE ${message.points} POINT JACKPOT!`);
+        }
       }
     });
     this.redisSubscriber.subscribe('discordmessage');
+    this.redisSubscriber.subscribe('prizemessage');
 
     this.discordClient
       .login(DISCORD_TOKEN)
@@ -55,18 +67,20 @@ export class DiscordBot {
         var args = commandBody.split(' ');
         var command = args.shift().toLowerCase();
         if (command === 'dance') {
-          msg.channel.send(':D\\-<');
-          msg.channel.send(':D|-<');
-          msg.channel.send(':D/-<');
-          this.redisPublisher.publish('chatmessage', JSON.stringify({message: {username: 'Fukutv Bot', chatmessage: ':D\\-<'}}), () => {});
-          this.redisPublisher.publish('chatmessage', JSON.stringify({message: {username: 'Fukutv Bot', chatmessage: ':D|-<'}}), () => {});
-          this.redisPublisher.publish('chatmessage', JSON.stringify({message: {username: 'Fukutv Bot', chatmessage: ':D/-<'}}), () => {});
+          this.chat(msg.channel, 'Fukutv Bot', ':D\\\\-<');
+          this.chat(msg.channel, 'Fukutv Bot', ':D|-<');
+          this.chat(msg.channel, 'Fukutv Bot', ':D/-<');
         }
       } else {
         this.redisPublisher.publish('chatmessage', JSON.stringify({message: {username: msg.author.username, chatmessage: msg.content}}), () => {});
       }
 
     });
+  }
+
+  chat(channel: any, username: any, message: any) {
+    channel.send(message);
+    this.redisPublisher.publish('chatmessage', JSON.stringify({message: {username: username, chatmessage: message}}));
   }
 }
 
