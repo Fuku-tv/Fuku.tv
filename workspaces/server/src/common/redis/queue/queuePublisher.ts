8cb3a52dfac2acdcf6a5dd386448c37ef489128a
blackfenix2 <devin_98@msn.com>
2021-06-22 16:37:52 -0400

    added basic get/set in queuePublisher.

--- workspaces/server/src/common/redis/queue/queuePublisher.ts
+++ workspaces/server/src/common/redis/queue/queuePublisher.ts
@@ -9,6 +9,29 @@ const queuePublisher = {
   publish(data: any): void {
     redisPublisher.publish(QUEUE_PUBLISH, data);
   },
+
+  async getQueue(): Promise<string> {
+    return new Promise<string>((resovle, reject) => {
+      redisPublisher.get(QUEUE_PUBLISH, (err: Error, reply: string) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        resovle(reply);
+      });
+    });
+  },
+  async setQueue(data: any): Promise<string> {
+    return new Promise<string>((resovle, reject) => {
+      redisPublisher.set(QUEUE_PUBLISH, data, (err: Error, reply: string) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        resovle(reply);
+      });
+    });
+  },
 };
 
 export default queuePublisher;
