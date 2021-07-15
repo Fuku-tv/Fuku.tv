7b42a11d16b05fb93d3ee2349f26861740ff6dec
blackfenix2 <devin_98@msn.com>
2021-07-15 13:24:45 -0400

    staged

--- workspaces/client/src/components/UIElements/ContentContainer/ContentContainer.tsx
+++ workspaces/client/src/components/UIElements/ContentContainer/ContentContainer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import * as styles from './ContentContainer.module.scss';
+import styles from './ContentContainer.module.scss';
 
-const ContentContainer: React.FC = (props) => <div className={styles['outer-content-wrapper']}>{props.children}</div>;
+const ContentContainer: React.FC = (props) => <div className={styles['test-weapper']}>{props.children}</div>;
 
 export default ContentContainer;
