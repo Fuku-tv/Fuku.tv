cf7485348dbd4b9d745247d9e45e66a22addd655
blackfenix2 <devin_98@msn.com>
2021-07-21 21:05:56 -0400

    BUMP

--- workspaces/client/src/index.ts
+++ workspaces/client/src/index.ts
@@ -4,16 +4,6 @@ import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import App from './App';
 
-Sentry.init({
-  dsn: 'https://b0a77ca8f00e4227a7b8d64c3c160f2a@o879857.ingest.sentry.io/5832905',
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
-
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
 
 interface HotModule {
@@ -25,3 +15,13 @@ interface HotModule {
 if ((module as HotModule).hot) {
   (module as HotModule).hot.accept();
 }
+
+// Sentry.init({
+//   dsn: 'https://b0a77ca8f00e4227a7b8d64c3c160f2a@o879857.ingest.sentry.io/5832905',
+//   integrations: [new Integrations.BrowserTracing()],
+
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 1.0,
+// });
