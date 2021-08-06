4b7a093f7bbdc84de40310fbd64b0e0284b53450
Ernie Francis <devin_98@msn.com>
2021-08-06 12:37:52 -0400

    changed server-side call to add nickname, fallback to split ID if undefined.

--- workspaces/lambda/src/http/leaderboard.ts
+++ workspaces/lambda/src/http/leaderboard.ts
@@ -7,9 +7,13 @@ export const index: APIGatewayProxyHandler = async (event, context, callback) =>
   const { domainName, stage, identity } = event.requestContext;
   const env = process.env.LAMBDA_ENV;
   try {
-    const players = await playersTableModel.getList(['nickname', 'points']);
+    const players = await playersTableModel.getList(['id', 'nickname', 'points']);
     const top20Players = players.slice(0, 20).sort((a, b) => b.points - a.points);
-    return Responses.ok({ playerList: top20Players });
+    const response = top20Players.map((player) => ({
+      nickname: player.nickname && player.id.split('@')[0],
+      points: player.points,
+    }));
+    return Responses.ok({ playerList: response });
   } catch (error) {
     callback(error);
     return Responses.badRequest({ message: 'message could not be received' });
