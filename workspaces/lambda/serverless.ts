e2068a7e5068e01c912df40a0e84f43edea190c6
blackfenix2 <devin_98@msn.com>
2021-08-08 00:09:11 -0400

    removed debug update_freeplay API

--- workspaces/lambda/serverless.ts
+++ workspaces/lambda/serverless.ts
@@ -102,12 +102,6 @@ const serverlessConfiguration: Serverless = {
       handler: 'src/trigger/scheduled/updateFreeplay.index',
       events: [
         {
-          http: {
-            path: '/trigger/updateFreeplay',
-            method: 'post',
-          },
-        },
-        {
           schedule: {
             rate: 'rate(4 hours)',
           },
