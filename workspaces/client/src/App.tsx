3ac477bde5e8c372a4cec75664f417ef1efd58d2
Ernie Francis <Devin_98@msn.com>
2022-04-08 22:20:26 -0400

    changed cookie notification text

--- workspaces/client/src/App.tsx
+++ workspaces/client/src/App.tsx
@@ -1,12 +1,52 @@
+import { css } from '@emotion/css';
 import * as React from 'react';
 import CookieConsent from 'react-cookie-consent';
 import { BrowserRouter as Router } from 'react-router-dom';
-import * as styles from './App.module.scss';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Provider from './state';
 
-const props = {};
+const styles = {
+  appBodyWrapper: css`
+    // min-height: 90vh;
+    position: absolute;
+    top: var(--headerHeight);
+    width: 100%;
+    height: calc(100% - var(--headerHeight));
+    main {
+      overflow-y: auto;
+      position: fixed;
+      width: calc(100% - var(--sideNavWidth));
+      top: var(--headerHeight);
+      width: 100%;
+      height: calc(100% - var(--headerHeight));
+      & > div {
+        height: 100%;
+        // padding-top: 30px;
+        // height: calc(100% - 30px);
+        // margin-top: 30px;
+        // padding: 10px 0;
+        // padding: 10px;
+      }
+    }
+    @media only screen and (max-width: 600px) {
+      main {
+        padding: 0px !important;
+        margin: 0 !important;
+        left: 0 !important;
+        width: 100% !important;
+      }
+
+      // .player-stats-container {
+      //   justify-content: space-between !important;
+      //   #level progress {
+      //     width: 80px !important;
+      //   }
+      // }
+    }
+  `,
+};
+
 const App: React.FC = () => (
   <Provider>
     <Router>
@@ -16,7 +56,7 @@ const App: React.FC = () => (
       </div>
     </Router>
     <CookieConsent style={{ textAlign: 'center', fontSize: '14px' }}>
-      This website uses cookies to enhance the user experience. And is only used by users 13 and older for COPPA lulz
+      This website uses cookies to enhance the user experience. This site is inteded for use by users 13 and older
     </CookieConsent>
   </Provider>
 );
