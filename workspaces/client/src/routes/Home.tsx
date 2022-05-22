e79d2752171f22eca16eedf102157ee3e0a8132c
Ernie Francis <Devin_98@msn.com>
2022-05-22 11:51:44 -0400

    added maintenance page

--- workspaces/client/src/routes/Home.tsx
+++ workspaces/client/src/routes/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeScreen from 'src/screens/homeScreen/HomeScreen';
+import MaintenanceScreen from 'src/screens/maintenanceScreen/MaintenanceScreen';
 
-const Home: React.FC = (props) => <HomeScreen />;
+const Home: React.FC = (props) => <MaintenanceScreen />;
 
 export default Home;
---
+++ workspaces/client/src/screens/maintenanceScreen/MaintenanceScreen.tsx
@@ -0,0 +1,25 @@
+import { Center, Container, Heading, Link, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const MaintenanceScreen = (props: Props) => (
+  <Container paddingTop={10}>
+    <Center flexDirection="column">
+      <Heading as="h1" marginY={10}>
+        Under Maintenance
+      </Heading>
+      <Text marginY={10}>
+        Our apologies, the clawgame is down until further notice. We are rebuilding everything on our end to accommodate our new location. This will
+        take a couple of weeks as we are in the process of moving. I will let you know when we are back up on our Discord channel{' '}
+      </Text>
+
+      <Text>
+        stay in touch with us at{' '}
+        <Link color="blue.400" textDecoration="underline" href="https://discord.gg/uNNEDt5JBX">
+          our Discord Link
+        </Link>
+      </Text>
+    </Center>
+  </Container>
+);
+
+export default MaintenanceScreen;
